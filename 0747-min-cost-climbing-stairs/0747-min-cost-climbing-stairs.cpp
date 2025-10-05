dad0dc21a96c39ce1387742afcf0b82ba9aa2911
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-10-05 19:28:01 +0530

    Time: 0 ms (100%), Space: 17.4 MB (89.6%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,19 +1,42 @@
+// *****RECURSION + MEMOIZATION*********
+
+            // class Solution {
+            // public:
+            // int t[1001];
+            //     int solve(int idx , vector<int> &cost)
+            //     {
+            //         if(idx>=cost.size()) return 0;
+
+            //         if(t[idx]!=-1) return t[idx];
+
+            //         int a = cost[idx] + solve(idx+1 , cost);
+            //         int b = cost[idx] +  solve(idx+2 , cost);
+
+            //         return t[idx] = min(a,b);
+            //     }
+            //     int minCostClimbingStairs(vector<int>& cost) {
+            //         memset(t , -1 , sizeof(t));
+            //         return min(solve(0 , cost) , solve(1,cost));
+            //     }
+            // };
+
+
+    //  *******BOTTOM UP APPROACH*********
 class Solution {
 public:
-int t[1001];
-    int solve(int idx , vector<int> &cost)
-    {
-        if(idx>=cost.size()) return 0;
-
-        if(t[idx]!=-1) return t[idx];
-
-        int a = cost[idx] + solve(idx+1 , cost);
-        int b = cost[idx] +  solve(idx+2 , cost);
-
-        return t[idx] = min(a,b);
-    }
     int minCostClimbingStairs(vector<int>& cost) {
-        memset(t , -1 , sizeof(t));
-        return min(solve(0 , cost) , solve(1,cost));
+
+        int n = cost.size();
+
+        if(n==2) 
+        return min(cost[0] , cost[1]);
+
+        for(int i = 2 ; i<n ; i++)
+        {
+            cost[i] = cost[i] + min(cost[i-1]  , cost[i-2]);
+        }
+
+        return min(cost[n-1] , cost[n-2]);
+        
     }
 };
