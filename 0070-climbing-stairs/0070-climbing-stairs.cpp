194149fe5415e2ad93313d7d8987c25f4b0714e1
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-10-05 18:06:16 +0530

    Time: 0 ms (100%), Space: 8.5 MB (36.09%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
-    int t[46];
-    int solve(int n)
-    {
-        if(n<0) return 0;
-
-        if(n==0) return 1;
-
-        if(t[n]!=-1)
-        return t[n];
-
-        return t[n]=solve(n-1) + solve(n-2);
-    }
     int climbStairs(int n) {
-        memset(t,-1 , sizeof(t));
-        return solve(n);
+        // BOTTOM UP APPROACH
+
+        if(n==1 || n==2 || n==3) return n;
+
+        vector<int> t(n+1); // n+1 size ka ak vector bna liya hai hmne
+        
+        t[0]=0; 
+        t[1]=1; 
+        t[2]=2; 
+
+        for(int i = 3 ; i<=n ; i++)
+        {
+            t[i] = t[i-1] + t[i-2];
+        }
+
+        return t[n];
     }
 };
