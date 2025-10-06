4ba1c08af0264288fbf23d1c48cb731a0b8ae79d
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2025-10-06 19:57:28 +0530

    Time: 0 ms (100%), Space: 11.1 MB (10.99%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,32 @@
+
+
+class Solution {
+public:
+int t[101];
+    int solve(int i , string s , int &n)
+    {
+         if(i>=n)
+         return 1;
+         if(s[i]=='0')
+         return 0;
+        //  
+        if(t[i]!=-1) return t[i];
+        // int result = 0;
+          t[i] = solve(i+1 , s , n);
+
+         if(i+1<n)
+         {
+            if(s[i]=='1' ||( s[i]=='2' && s[i+1]<='6'))
+            {
+                t[i]+=solve(i+2 , s , n);
+            }
+         }
+         return t[i];
+    }
+    int numDecodings(string s) {
+        memset(t,-1,sizeof(t));
+        int n = s.size();
+       return solve(0 , s , n);
+
+    }
+};
