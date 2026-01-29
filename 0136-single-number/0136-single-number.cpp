0c8b2c1fe28801b90e53e2d08540b42586deb7ce
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-01-29 22:41:25 +0530

    Time: 0 ms (100%), Space: 20.6 MB (82.6%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        sort(nums.begin() , nums.end());
 
-      
-        for(int i = 0 ; i<nums.size()-1 ; i+=2)
+        // using xor
+        // in xor  a ^ a = 0  , a ^ 0 = a ;
+        // same pe zero 
+        
+        int ans = 0 ;
+        for(int i = 0 ;i<nums.size() ; i++)
         {
-            if(nums[i]!=nums[i+1])
-            return nums[i];
+             ans = ans^nums[i];
         }
 
-        return nums.back();
+        return ans;
     }
 };
