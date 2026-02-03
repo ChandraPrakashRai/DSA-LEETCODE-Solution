3e385d4b389208e8b926f413bb358bafbf5cdc23
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-02-03 11:21:40 +0530

    Time: 0 ms (100%), Space: 10.8 MB (38.66%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,28 +1,32 @@
 class Solution {
 public:
+    int maxAmount(vector<int>& nums, int index, vector<int>& dp) {
+        if (index < 0) return 0;
+        if (index == 0) return nums[0];
 
-        int find(int index ,vector<int>&nums , int n , vector<int>& dp){
-            if(index>=n)
-            {
-                return 0;
-            }
-
-            if(dp[index]!=-1)
+        if (dp[index] != -1)
             return dp[index];
-           
-           return dp[index] =  max(nums[index] + find(index+2 , nums , n , dp) , find(index+1 , nums , n , dp));
 
+        int take = nums[index] + maxAmount(nums, index - 2, dp);
+        int skip = maxAmount(nums, index - 1, dp);
 
+        return dp[index] = max(take, skip);
+    }
 
-        }
     int rob(vector<int>& nums) {
-       
         int n = nums.size();
-        if(n==1)
-        return nums[0];
-        vector<int>dp1(n+2 , -1);
-        vector<int>dp2(n+2 , -1);
-        int index = 0;
-       return max(find( 0 ,nums , n-1  , dp1) , find(1 , nums , n ,dp2));
+        if (n == 1) return nums[0];
+
+        // case 1: houses 0 to n-2
+        vector<int> nums1(nums.begin(), nums.end() - 1);
+        vector<int> dp1(nums1.size(), -1);
+        int first_case = maxAmount(nums1, nums1.size() - 1, dp1);
+
+        // case 2: houses 1 to n-1
+        vector<int> nums2(nums.begin() + 1, nums.end());
+        vector<int> dp2(nums2.size(), -1);
+        int second_case = maxAmount(nums2, nums2.size() - 1, dp2);
+
+        return max(first_case, second_case);
     }
 };
