6d5c9b5a4cc77966f69d5fefbaadd1847b227271
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-02-05 20:00:27 +0530

    Time: 7 ms (87.31%), Space: 16.8 MB (36.89%) - LeetHub

---
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+int LCS(string &s1 , string &s2 , int index1 , int index2 , vector<vector<int>>& dp)
+{
+    // base case
+    if(index1<0 || index2<0)
+    return 0;
+
+    if(dp[index1][index2]!=-1)
+    return dp[index1][index2];
+    
+    if(s1[index1]==s2[index2])
+    {
+        return  dp[index1][index2] =  1 + LCS(s1 , s2 , index1-1 , index2-1 , dp);
+    }
+    
+    else
+    return  dp[index1][index2] = max(LCS(s1 , s2 , index1 , index2-1 , dp) , LCS(s1 , s2 , index1-1 , index2 , dp));
+  }
+    int minDistance(string s1, string s2) {
+             int n1 = s1.size() ;
+        int n2 = s2.size();
+      vector<vector<int>>dp(n1 , vector<int>(n2 , -1));
+      int lcs = LCS(s1 , s2 , n1-1 , n2-1 , dp);
+
+      return ((n1 - lcs ) + (n2-lcs));
+    }
+};
