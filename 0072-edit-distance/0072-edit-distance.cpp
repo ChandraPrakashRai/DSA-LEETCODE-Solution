b2266d3eb272c46176b726ca60e34bfb90e2527c
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-02-06 10:33:34 +0530

    Time: 60 ms (12.13%), Space: 84.4 MB (5.25%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,29 +1,52 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        int n = word1.size();
-        int m = word2.size();
-
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-
-        // base cases
-        for (int i = 0; i <= n; i++) dp[i][0] = i;
-        for (int j = 0; j <= m; j++) dp[0][j] = j;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (word1[i - 1] == word2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = 1 + min({
-                        dp[i - 1][j],     // delete
-                        dp[i][j - 1],     // insert
-                        dp[i - 1][j - 1]  // replace
-                    });
-                }
-            }
+    int editDist(string s1 , string s2 , int n1 , int n2 , int i , int j , vector<vector<int>>&dp)
+    {
+        // base case
+        if(i<0)
+        {
+            return  j+1;
+        }
+        if(j<0)
+        {
+            return   i+1;
         }
 
-        return dp[n][m];
+        if(dp[i][j]!=-1)
+        return dp[i][j];
+
+        if(s1[i]==s2[j])
+        {
+            return dp[i][j]= editDist(s1 , s2 , n1 , n2 , i-1 , j-1 , dp);
+        }
+        else
+        {
+         return  dp[i][j] = 1+  min(
+            {
+            editDist(s1 , s2 , n1 , n2 ,i-1 , j ,dp) ,// delete
+            editDist(s1 , s2 , n1 , n2 ,i , j-1 , dp),  //insert
+            editDist(s1 , s2 , n1 , n2 ,i-1 , j-1 , dp)  // replace
+             });
+        }
+    }
+    int minDistance(string word1, string word2) {
+        
+        int n1 = word1.size();
+        int n2 = word2.size();
+
+        if(n1==0 && n2==0)
+        return 0;
+
+        if(n1 && !n2)
+        return n1;
+
+        if(n2 && !n1)
+        return n2;
+
+
+        vector<vector<int>>dp(n1, vector<int>(n2 , -1));
+
+        return editDist(word1 , word2 , n1 , n2 , n1-1 , n2-1 , dp);
+
     }
 };
