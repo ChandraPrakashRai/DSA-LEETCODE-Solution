fe4a67c8a4da646f6081349dba6dd0efc397cb23
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-02-07 23:25:20 +0530

    Time: 0 ms (100%), Space: 17.5 MB (87.13%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,61 @@
 class Solution {
 public:
+
+      int BS2(vector<int>& nums, int target , int n)
+    {
+            int second = -1;
+            int start = 0;
+            int end = nums.size()-1 ;
+            
+            while(start<=end)
+            {
+                int mid = start + (end - start) / 2;
+
+                if(nums[mid]==target)
+                {
+                    second = mid;
+                    start = mid+1;
+                }
+                else if(nums[mid]<target)
+                {
+                    start = mid+1;
+                }
+                else
+                end  = mid-1;
+            }
+
+            return second;
+
+    }
+
+    int BS(vector<int>& nums, int target , int n)
+    {
+            int first = -1;
+            int start = 0;
+            int end = nums.size()-1 ;
+            
+            while(start<=end)
+            {
+                int mid = start + (end - start) / 2;
+
+                if(nums[mid]==target)
+                {
+                    first = mid;
+                    end = mid-1;
+                }
+                else if(nums[mid]<target)
+                {
+                    start = mid+1;
+                }
+                else
+                end  = mid-1;
+            }
+        return first;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int first = -1;
-        int last = -1;
-        int start = 0;
-        int end = n-1;
-        vector<int>ans;
-
-        while(start <= end)
-        {
-            int mid = start + (end-start)/2;
-
-            if(nums[mid]==target)
-            {
-                first = mid;
-                end = mid-1;
-            }
-            else if(nums[mid] < target)
-            {
-                start = mid + 1;
-            }
-            else 
-            end = mid-1;
-        }
-
-
-        //  for last occured index
-        start = 0;
-        end = n-1;
-        while(start <= end)
-        {
-            int mid = start + (end-start)/2;
-
-            if(nums[mid]==target)
-            {
-                last = mid;
-                start = mid+1;
-            }
-            else if(nums[mid] < target)
-            {
-                start = mid+1;
-            }
-            else 
-            end = mid-1;
-        }
-       return {first , last};
+       int n = nums.size();
+       int first = BS(nums , target , n);
+       int second = BS2(nums , target , n);
+        return {first,second};
     }
 };
