d3e60453e3d0240698bc07b01f025e187dd252d9
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-02-14 16:38:08 +0530

    Time: 1 ms (41.83%), Space: 16.4 MB (41.37%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,35 +1,31 @@
 class Solution {
 public:
 
-    int count(int m , int n , vector<vector<int>>& grid , int i , int j  , vector<vector<int>>& dp){
-        if(i==m-1 && j==n-1)
-        return grid[i][j];
+int minSum(vector<vector<int>>&grid , int i , int j , vector<vector<int>>&dp)
+{
+    // base case
+    if(i==0 && j==0)
+    return dp[i][j] = grid[i][j];
+    
+     if (i < 0 || j < 0)
+        return 1e9;
+    
 
-        if(dp[i][j]!=-1)
-        return dp[i][j] ;
+    if(dp[i][j]!=-1)
+    return dp[i][j];
+    //recursive case
 
-        if(i==m-1)
-        {
-         return   grid[i][j] + count(m , n , grid , i , j+1 , dp);
-        }
-
-        if(j==n-1)
-        {
-          return  grid[i][j] + count(m , n , grid , i+1 , j , dp);
-        }
-        
-        int down = count(m , n , grid , i+1 , j , dp);
-
-        int right = count(m , n , grid , i , j+1, dp);
-
-        return  dp[i][j] = grid[i][j] + min(down , right);
-    }
+     
+     int up = grid[i][j] +  minSum(grid , i-1 , j , dp);
+     int left = grid[i][j]  + minSum(grid , i , j-1 , dp);
+     
+     return dp[i][j] = min(up , left);
+    
+}
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-
-        vector<vector<int>>dp(m , vector<int>(n , -1));
-
-        return count(m , n,grid , 0 , 0 , dp);
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>>dp(n , vector<int>(m, -1));
+        return minSum(grid , n-1 , m-1 , dp);
     }
 };
