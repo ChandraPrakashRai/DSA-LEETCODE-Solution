7870448d2c478e8a38ae3d5b8ccb28fc1818c530
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-02-14 15:02:36 +0530

    Time: 0 ms (100%), Space: 11 MB (7.61%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
-    int maxAmount(vector<int>& nums, int index, vector<int>& dp) {
-        if (index < 0) return 0;
-        if (index == 0) return nums[0];
+     int  cost(vector<int>&nums , int index , vector<int>&dp)
+     {
+        // base case
+        if(index==0)
+        return nums[0];
 
-        if (dp[index] != -1)
-            return dp[index];
+        if(index<1) return 0;
 
-        int take = nums[index] + maxAmount(nums, index - 2, dp);
-        int skip = maxAmount(nums, index - 1, dp);
+        if(dp[index]!=-1) return dp[index];
 
-        return dp[index] = max(take, skip);
-    }
+        int pick = nums[index] + cost(nums , index-2  , dp);
 
+        int notpick = 0 + cost(nums , index-1 , dp);
+
+        return  dp[index] =  max(pick , notpick);
+     }
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return nums[0];
+       if(nums.size()==1)
+       return nums[0];
+        
+        //first case
+        vector<int>nums1(nums.begin() , nums.end()-1);
+        vector<int>dp1(nums1.size() ,-1);
+        //second case
+        vector<int>nums2(nums.begin()+1 , nums.end());
+        vector<int>dp2(nums2.size(),-1);
 
-        // case 1: houses 0 to n-2
-        vector<int> nums1(nums.begin(), nums.end() - 1);
-        vector<int> dp1(nums1.size(), -1);
-        int first_case = maxAmount(nums1, nums1.size() - 1, dp1);
+        int first = cost(nums1 , nums1.size()-1 , dp1);
+        int second = cost(nums2 , nums2.size()-1, dp2);
 
-        // case 2: houses 1 to n-1
-        vector<int> nums2(nums.begin() + 1, nums.end());
-        vector<int> dp2(nums2.size(), -1);
-        int second_case = maxAmount(nums2, nums2.size() - 1, dp2);
-
-        return max(first_case, second_case);
+        return max(first , second);
+        
     }
 };
