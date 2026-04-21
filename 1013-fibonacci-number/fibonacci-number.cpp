11512180b9c5c80103fb6249df7f3752470a8eea
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-04-22 01:10:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-int t[31];
-
-int fibo(int n)
-{
-     if(n==0)
-        return 0;
-
-        if(n==1)
-        return 1;
-
-        if(t[n]!=-1)
-        return t[n];
-
-        return t[n] =  fibo(n-1)+ fibo(n-2);
-}
     int fib(int n) {
-
-        memset(t, -1 , sizeof(t));
         
+        if(n<=1)
+        {
+            return n;
+        }
 
-        return fibo(n);
+        // create a dp of size(n+1)
+        vector<int>dp(n+1);
+
+        // fill the dp with the base cases
+        dp[0]= 0;
+        dp[1]=1;
+
+        for(int i = 2 ; i<=n ; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
