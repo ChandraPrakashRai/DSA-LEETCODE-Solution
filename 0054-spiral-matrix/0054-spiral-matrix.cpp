dac00c7ac6acc743792d9a50011d04c17dd11281
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-05-06 03:40:16 +0530

    Time: 0 ms (100%), Space: 9.4 MB (50.72%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,48 +1,46 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int>ans;
-        int row = matrix.size();
-        int col = matrix[0].size();
-        int left=0, right=col-1 , bottom=row-1 , top =0;
-        while(top<=bottom&& left<=right)
-        {
-            //top
-            for(int j =left ; j<=right ; j++)
-            {
-                ans.push_back(matrix[top][j]);
-                
-               }
-               top++;
-               // right
-            for(int i=top ; i <=bottom ; i++)
-            {
-                ans.push_back(matrix[i][right]);
-             
+        int n = matrix.size();
+        int m = matrix[0].size();
 
+        vector<int> res;
+
+        int top = 0, bottom = n - 1;
+        int left = 0, right = m - 1;
+
+        int dir = 0;
+
+        while (top <= bottom && left <= right) {
+
+            if (dir == 0) { // left -> right
+                for (int i = left; i <= right; i++) {
+                    res.push_back(matrix[top][i]);
+                }
+                top++;
             }
-               right--;
-            //bottom
-            if(top<=bottom) 
-           { 
-            for(int j =right ; j>=left ; j--)
-            ans.push_back(matrix[bottom][j]);
-        
+            else if (dir == 1) { // top -> bottom
+                for (int i = top; i <= bottom; i++) {
+                    res.push_back(matrix[i][right]);
+                }
+                right--;
             }
+            else if (dir == 2) { // right -> left
+                for (int i = right; i >= left; i--) {
+                    res.push_back(matrix[bottom][i]);
+                }
                 bottom--;
-
-            // left
-            if(left<=right)
-            {
-            
-            
-            for(int i = bottom ; i >=top ; i--)
-            ans.push_back(matrix[i][left]);
-        
             }
+            else if (dir == 3) { // bottom -> top
+                for (int i = bottom; i >= top; i--) {
+                    res.push_back(matrix[i][left]);
+                }
                 left++;
+            }
+
+            dir = (dir + 1) % 4; // change direction
         }
-        return ans;
+
+        return res;
     }
-    
 };
