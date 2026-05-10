f8b276fcdf9209c5c25c6c25ca10ecb0ff4f9a3d
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-05-11 02:47:35 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (81.77%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,45 +1,20 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int n = nums.size();
 
-        vector<int>arr1;
-        vector<int>arr2;
 
-        for(int i = 0 ; i<n ; i++)
-        {
-            if(nums[i]==0)
+      int left = 0;
+      int right = 0;
+
+            while(right < nums.size())
             {
-                arr2.push_back(nums[i]);
+                if(nums[right]!=0)
+                {
+                    swap(nums[left] , nums[right]);
+                    left++;
+                    
+                }
+                right++;
             }
-            else
-            {
-                arr1.push_back(nums[i]);
-            }
-        }
-
-        vector<int>res(arr1.size() + arr2.size());
-        
-       int n1 = arr1.size();
-       int i = 0;
-       while(i<n1)
-       {
-        res[i]=arr1[i];
-        i++;
-       }
-
-        int n2 = arr2.size();
-       int j = 0;
-       while(j<n2)
-       {
-        res[i+j]=arr2[j];
-        j++;
-       }
-
-      for(int i = 0 ; i<res.size() ; i++)
-      {
-        nums[i] = res[i];
-      }
-
     }
 };
