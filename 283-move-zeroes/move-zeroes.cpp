cd7d70a8c5639e4147e5a0053f3e2584c4243a59
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-05-10 22:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (5.21%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,27 +1,45 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
-        // optimized code 
+        int n = nums.size();
 
-        // use two pointer approach
+        vector<int>arr1;
+        vector<int>arr2;
 
-        int left = 0 ;
-        int right = 0;
-
-
-        for(int i = 0 ; i<nums.size() ; i++)
+        for(int i = 0 ; i<n ; i++)
         {
-
-            if(nums[right]!=0)
+            if(nums[i]==0)
             {
-                swap(nums[left] , nums[right]);
-                right++;
-                left++;
+                arr2.push_back(nums[i]);
             }
             else
-            right++;
-
+            {
+                arr1.push_back(nums[i]);
+            }
         }
+
+        vector<int>res(arr1.size() + arr2.size());
+        
+       int n1 = arr1.size();
+       int i = 0;
+       while(i<n1)
+       {
+        res[i]=arr1[i];
+        i++;
+       }
+
+        int n2 = arr2.size();
+       int j = 0;
+       while(j<n2)
+       {
+        res[i+j]=arr2[j];
+        j++;
+       }
+
+      for(int i = 0 ; i<res.size() ; i++)
+      {
+        nums[i] = res[i];
+      }
+
     }
 };
