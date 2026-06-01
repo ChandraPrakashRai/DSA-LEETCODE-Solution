95e1ef144b14b162a73ed1502fd3a95ba0c2fc64
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-06-01 23:11:57 +0530

    Time: 2 ms (19.22%) | Memory: 28.4 MB (6.35%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,43 +1,35 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        // two pointer approach
-
+        
         int n = height.size();
+        // using prefix and suffix 
+        vector<int>prefix;
+        vector<int>suffix;
 
         int water = 0;
 
-         int left = 0;
-         int right = n-1;
-
-
-          int leftMax = 0;
-          int rightMax = 0;
-
-       while(left < right)
+        int left = INT_MIN;
+        int right = INT_MIN;
+        for(int i = 0 ; i<height.size()  ; i++)
         {
+            left = max(left , height[i]);
+            prefix.push_back(left);
+        }
+        for(int i =height.size()-1 ; i>=0  ; i--)
+        {
+            right = max(right , height[i]);
+            suffix.push_back(right);
+        }
 
-            leftMax = max(leftMax , height[left]);
-            rightMax = max(rightMax , height[right]);
+        reverse(suffix.begin() , suffix.end());
 
-
-          if(leftMax < rightMax)
-          {
-            water = water + leftMax - height[left];
-            left++;
-
-
-          }
-          else
-          {
-           water = water + rightMax- height[right];
-            right--;
-
-          }
-
-          
+        for(int i = 0; i<height.size() ; i++)
+        {
+            water += min(prefix[i] , suffix[i])-height[i];
         }
 
         return water;
+
     }
 };
