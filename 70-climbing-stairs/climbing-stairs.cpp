43e2c00d62334051d32b10fc33fea909e676a1ed
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-06-27 23:43:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (18.27%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -7,15 +7,20 @@ int stairs(int n , vector<int>&dp)
 
     if(n==1) return 1;
 
-    if(dp[n]!=-1)
-    return dp[n];
+    dp[0] = 1;
+    dp[1] = 1;
 
-    return dp[n] = stairs(n-1 , dp) + stairs(n-2 , dp);
+    for(int i =2 ; i<=n ; i++)
+    {
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
 }
     int climbStairs(int n) {
         
         // dp
-        vector<int>dp(n+1 , -1);
+        vector<int>dp(n+1);
 
         return stairs(n , dp);
     }
