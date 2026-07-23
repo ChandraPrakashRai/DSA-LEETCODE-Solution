925ac2155597cbd7032873ed0dd55c99e7867fbe
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-07-24 00:20:43 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (34.93%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        
+        // Trapping of Rain Watre ka sbse Best Way 
+        // and Easy Way hai
+        // Prefix (LeftMax) 
+        // and Suffix(RightMax) method kahte hai
+
+        // pREFIX(lEFTmaX)
         int n = height.size();
-        // using prefix and suffix 
-        vector<int>prefix;
-        vector<int>suffix;
+        vector<int>prefix(n);
+        prefix[0] = height[0];
+
+        for(int i = 1 ; i<height.size() ; i++)
+        {
+            prefix[i] = max(prefix[i-1] , height[i]);
+        }
+
+        vector<int>suffix(n);
+        suffix[n-1] = height[n-1];
+
+        for(int i = n-2 ; i>=0 ; i--)
+        {
+            suffix[i] = max(suffix[i+1] , height[i]);
+        }
 
         int water = 0;
 
-        int left = INT_MIN;
-        int right = INT_MIN;
-        for(int i = 0 ; i<height.size()  ; i++)
+        for(int i = 0 ; i<n ; i++)
         {
-            left = max(left , height[i]);
-            prefix.push_back(left);
-        }
-        for(int i =height.size()-1 ; i>=0  ; i--)
-        {
-            right = max(right , height[i]);
-            suffix.push_back(right);
+            water = water + min(prefix[i] , suffix[i]) - height[i];
         }
 
-        reverse(suffix.begin() , suffix.end());
-
-        for(int i = 0; i<height.size() ; i++)
-        {
-            water += min(prefix[i] , suffix[i])-height[i];
-        }
 
         return water;
-
     }
 };
