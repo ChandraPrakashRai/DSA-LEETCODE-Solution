1bb6368a5f3ecae525b7a41ce95b1208965d3168
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-08-12 02:49:29 +0530

    Time: 329 ms (5.64%) | Memory: 149.8 MB (5.02%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
 
-    void solve(vector<int>& nums , int idx , vector<vector<int>>
-    &result )
+vector<vector<int>>perm(vector<int>nums , int idx , vector<vector<int>>&ans , int n)
+{
+    if(idx==n)
     {
-        if(idx ==nums.size())
-        {
-            result.push_back(nums);
-            return;
-        }
-
-       
-           for( int i = idx ; i< nums.size() ; i++)
-           {
-             swap(nums[idx] , nums[i]);
-             solve(nums , idx+1 , result);
-             swap(nums[idx] , nums[i]);
-
-
-           }
-        
+        ans.push_back(nums);
+    
     }
-    vector<vector<int>> permute(vector<int>& nums) {
-        int idx = 0;
-        vector<vector<int>>result;
-        solve(nums , idx , result );
 
-        return result;
+    //recusrsive code
+    for(int i = idx ; i<n ; i++)
+    {
+        swap(nums[i] , nums[idx]);
+        perm(nums , idx+1 , ans , n);
+
+        //backtrack
+        swap(nums[i] , nums[idx]);
+    }
+
+    return ans;
+}
+    vector<vector<int>> permute(vector<int>& nums) {
+        
+        vector<vector<int>>ans;
+        int n = nums.size();
+
+       return  perm(nums , 0 , ans , n);
     }
 };
