924e26bca2a7dd0678728f2c3da55080e8e9f87d
Chandra Prakash Rai <145516621+ChandraPrakashRai@users.noreply.github.com>
2026-08-12 02:51:39 +0530

    Time: 3 ms (25.90%) | Memory: 11.7 MB (13.68%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
 
-vector<vector<int>>perm(vector<int>nums , int idx , vector<vector<int>>&ans , int n)
+void perm(vector<int>nums , int idx , vector<vector<int>>&ans , int n)
 {
     if(idx==n)
     {
         ans.push_back(nums);
+        return;
     
     }
 
@@ -19,13 +20,14 @@ vector<vector<int>>perm(vector<int>nums , int idx , vector<vector<int>>&ans , in
         swap(nums[i] , nums[idx]);
     }
 
-    return ans;
+    // return ans;
 }
     vector<vector<int>> permute(vector<int>& nums) {
         
         vector<vector<int>>ans;
         int n = nums.size();
 
-       return  perm(nums , 0 , ans , n);
+         perm(nums , 0 , ans , n);
+         return ans;
     }
 };
